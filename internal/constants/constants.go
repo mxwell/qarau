aa9b1976b374940fa59149fbf85073ba8e16cbe3
Murat Khairulin <murat.khairulin@gmail.com>
2026-07-18 17:41:02 +0500

    Bump max duration: 40 -> 90 minutes

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 import "time"
 
 const (
-	MaxDurationSecs         = 40 * 60 // let's start with a lower limit - 40 minutes
+	MaxDurationSecs         = 90 * 60 // 1.5 hours
 	MinLikesCount           = 20
 	MinViewsCount           = 1000
 	AsrApiDailyQuotaSeconds = 10 * 60 * 60 // 10 hours per day
